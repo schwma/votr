b4779efa1dc7ddb259b5d0a90fb93ba47cdc7e68
Marcel Schwarz <37244550+schwma@users.noreply.github.com>
2019-02-16 18:12:15 +0100

    Add sequelize config file

--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/sequelize.json')[env];
 const db = {};
 
-let sequelize = new Sequelize('sqlite://polls.db', {
-  operatorsAliases: false,
-  // Disable showing executed sql queries on stdout
-  //logging: false,
-  define: {
-    timestamps: false,
-  },
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
